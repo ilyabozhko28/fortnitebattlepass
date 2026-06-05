4fd0d8e65924f64bc8188bd3046c52032e93da4d
ilyabozhko28 <ilya.bozhko28@fizmat.kz>
2026-06-05 09:49:40 +0500

    Set base path for Vite configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,11 +4,15 @@ import path from "node:path";
 
 export default defineConfig({
   plugins: [react()],
+
+  base: "/fortnitebattlepass/",
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
+
   server: {
     host: "0.0.0.0",
     port: 5173,
